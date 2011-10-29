5185ff4290bfe4f94e5ec0c0b0e67e9136a39f4d
Nesta <nguerrero@emergya.com>
2011-10-29 17:09:16 +0200

    [nguerrero] coder reviews fix

--- brutus/templates/page.tpl.php
+++ brutus/templates/page.tpl.php
@@ -116,9 +116,9 @@
     </div> <!-- /header -->
 
     <?php if (!empty($main_menu) || !empty($secondary_menu)): ?>
-      <div id="navigation" class="menu <?php if (!empty($main_menu)) { print "with-main-menu"; } if (!empty($secondary_menu)) { print " with-sub-menu"; } ?>">
-        <?php if (!empty($main_menu)){ print theme('links', $main_menu, array('id' => 'primary', 'class' => 'links main-menu')); } ?>
-        <?php if (!empty($secondary_menu)){ print theme('links', $secondary_menu, array('id' => 'secondary', 'class' => 'links sub-menu')); } ?>
+      <div id="navigation" class="menu <?php if (!empty($main_menu)) : print "with-main-menu"; endif; if (!empty($secondary_menu)) : print " with-sub-menu"; endif; ?>">
+        <?php if (!empty($main_menu)): print theme('links', $main_menu, array('id' => 'primary', 'class' => 'links main-menu')); endif; ?>
+        <?php if (!empty($secondary_menu)): print theme('links', $secondary_menu, array('id' => 'secondary', 'class' => 'links sub-menu')); endif; ?>
       </div> <!-- /navigation -->
     <?php endif; ?>
 
